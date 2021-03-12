d6517b2edcac792ae73900f6e68a4c3f37cdbb8c
Dżesika Szymańska <dzesszy738@student.polsl.pl>
2021-03-12 16:44:09 +0100

    Commit 5

--- src/main.cpp
+++ src/main.cpp
@@ -11,21 +11,15 @@ int main(){
 
 
 #ifdef USE_TRIGONOMETRY_DEGREE
-	double wynik_sinus = degreemath::sinus(10);
-	double wynik_cosinus = degreemath::cosinus(10);
-	double wynik_tangens = degreemath::tangens(10);
-	double wynik_cotangens = degreemath::cotangens(10);
+	double wynik_sinus = degreemath::sinus(45);
+
 #else
 	double wynik_sinus = sin(M_PI/10);
-	double wynik_cosinus = cos(M_PI / 10);
-	double wynik_tangens = tan(M_PI / 10);
-	double wynik_cotangens = 1/tan(M_PI / 10);
+	
 #endif
 	
 	std::cout << wynik_sinus << std::endl;
-	std::cout << wynik_cosinus << std::endl;
-	std::cout << wynik_tangens << std::endl;
-	std::cout << wynik_cotangens << std::endl;
+	
 
 	return 0;
 }
